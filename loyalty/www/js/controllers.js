5479885bc7651d9ea99c57041c927bb14f8e63bb
Jaco Bezuidenhout <jaco@peoplesoft.co.za>
2015-09-01 17:48:33 +0200

    Init commit

---
+++ loyalty/www/js/controllers.js
@@ -0,0 +1,38 @@
+angular.module('starter.controllers', [])
+
+.controller('DashCtrl', function($scope, $cordovaBarcodeScanner) {
+  $scope.scanBarcode = function() {
+      $cordovaBarcodeScanner.scan().then(function(imageData) {
+          alert(imageData.text);
+          console.log("Barcode Format -> " + imageData.format);
+          console.log("Cancelled -> " + imageData.cancelled);
+      }, function(error) {
+          console.log("An error happened -> " + error);
+      });
+  };
+})
+
+.controller('ChatsCtrl', function($scope, Chats) {
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+
+  $scope.chats = Chats.all();
+  $scope.remove = function(chat) {
+    Chats.remove(chat);
+  };
+})
+
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+})
+
+.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+});
