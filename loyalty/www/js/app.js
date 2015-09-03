2dc49b625302ca8f0bda2205bad0173c12be84e4
Jaco Bezuidenhout <jaco@peoplesoft.co.za>
2015-09-03 12:00:47 +0200

    added flow for new transaction and new member

--- loyalty/www/js/app.js
+++ loyalty/www/js/app.js
@@ -1,23 +1,14 @@
-// Ionic Starter App
+var url = "http://10.0.0.120:1337";
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-// 'starter.services' is found in services.js
-// 'starter.controllers' is found in controllers.js
 angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', 'ngCordova'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
     if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
       cordova.plugins.Keyboard.disableScroll(true);
-
     }
     if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
       StatusBar.styleLightContent();
     }
   });
@@ -25,21 +16,12 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', '
 
 .config(function($stateProvider, $urlRouterProvider) {
 
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
   $stateProvider
-
-  // setup an abstract state for the tabs directive
   .state('tab', {
     url: '/tab',
     abstract: true,
     templateUrl: 'templates/tabs.html'
   })
-
-  // Each tab has its own nav history stack:
-
   .state('tab.dash', {
     url: '/dash',
     views: {
@@ -49,26 +31,15 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', '
       }
     }
   })
-
-  .state('tab.chats', {
-      url: '/chats',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/tab-chats.html',
-          controller: 'ChatsCtrl'
-        }
+  .state('tab.newMember', {
+    url: '/newMember',
+    views: {
+      'tab-newMember': {
+        templateUrl: 'templates/tab-newMember.html',
+        controller: 'newMemberCtrl'
       }
-    })
-    .state('tab.chat-detail', {
-      url: '/chats/:chatId',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/chat-detail.html',
-          controller: 'ChatDetailCtrl'
-        }
-      }
-    })
-
+    }
+  })
   .state('tab.account', {
     url: '/account',
     views: {
@@ -78,8 +49,6 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', '
       }
     }
   });
-
-  // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/tab/dash');
 
 });
--- loyalty/www/js/controllers.js
+++ loyalty/www/js/controllers.js
@@ -1,32 +1,207 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope, $cordovaBarcodeScanner) {
-  $scope.scanBarcode = function() {
-      $cordovaBarcodeScanner.scan().then(function(imageData) {
-          alert(imageData.text);
-          console.log("Barcode Format -> " + imageData.format);
-          console.log("Cancelled -> " + imageData.cancelled);
-      }, function(error) {
-          console.log("An error happened -> " + error);
-      });
-  };
+.controller('DashCtrl', function($scope,$http,$cordovaBarcodeScanner,$cordovaPinDialog,$ionicLoading,$ionicPopup,$location) {
+
+  $scope.error = function(m)
+  {
+    $scope.hide(); 
+    var alertPopup = $ionicPopup.alert({
+      title: 'Error',
+      template: '<pre><code>' + m + '</code></pre>'
+    });
+    alertPopup.then(function(res) {
+      console.log('Thank you for not eating my delicious ice cream cone');
+    });
+    $timeout(function() {
+       alertPopup.close(); //close the popup after 3 seconds for some reason
+    }, 3000);
+  }
+
+  $scope.success = function()
+  {
+    var myPopup = $ionicPopup.show({
+      template: '',
+      title: 'Success',
+      subTitle: 'Transaction added successfully.',
+      scope: $scope,
+      buttons: [
+        {
+          text: '<b>Ok</b>'
+        }
+      ]
+    });
+    // $timeout(function() {
+    //    myPopup.close(); //close the popup after 3 seconds for some reason
+    // }, 3000);
+  }
+
+  $scope.transaction = function()
+  {
+    
+    $cordovaBarcodeScanner.scan().then(function(qrMember) {
+      var t = {};
+      t.trans = {};
+      $scope.t = t;
+      
+      if (qrMember.text.length > 0 && !qrMember.cancelled)
+      {
+        t.qrMember = qrMember;
+        var id = JSON.parse(qrMember.text.replace("id",'"id"').replace("deviceId",'"deviceId"')).id;
+        $http.get(url + "/member/" + id).then(function(response){
+
+          var myPopup = $ionicPopup.show({
+            templateUrl: "./templates/transactionForm.html",
+            title: 'Transaction Info',
+            subTitle: 'Check information',
+            scope: $scope,
+            buttons: [
+              { text: 'Cancel' },
+              {
+                text: '<b>Save</b>',
+                type: 'button-positive',
+                onTap: function(e) {
+                  console.log(e);
+                  // t.check = $scope.trans.check;
+                  // t.amount = $scope.trans.amount;
+                  alert(JSON.stringify(t));
+                  $cordovaBarcodeScanner.scan().then(function(qrAdmin) {
+                    t.qrAdmin = qrAdmin;
+                    $http.post(url + "/member/" + response.data.id + "/transactions/add/",t)
+                    .then(function(response){
+                      $scope.success();
+                    },function(error){
+                      $scope.error(JSON.stringify(error,null,4));
+                    });
+                  },function(error) {
+                    $scope.error(JSON.stringify(error,null,4));
+                  });
+                }
+              }
+            ]
+          });
+        
+          myPopup.then(function(res) {
+            console.log('Tapped!', res);
+          });
+
+        },function(error){$scope.error(JSON.stringify(error,null,4));});
+      }
+      else
+      {
+        $scope.error("QR code scanning cancelled");
+      }
+    },function(error) {
+        $scope.error(JSON.stringify(error,null,4));
+    });
 
   
+
+
+  }
+
 })
 
-.controller('ChatsCtrl', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
-
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
+.controller('newMemberCtrl', function($scope,$http,$cordovaBarcodeScanner,$cordovaPinDialog,$ionicLoading,$ionicPopup,$location) {
+  $scope.show = function() {
+    $ionicLoading.show({
+      template: '<ion-spinner></ion-spinner>'
+    });
   };
+  $scope.hide = function(){
+    $ionicLoading.hide();
+  };
+
+  $scope.success = function()
+  {
+    var myPopup = $ionicPopup.show({
+      template: '',
+      title: 'Success',
+      subTitle: 'Member added successfully.',
+      scope: $scope,
+      buttons: [
+        {
+          text: '<b>Ok</b>',
+          type: 'button-balanced',
+          onTap: function(e) {
+            $location.path("/dash");
+          }
+        },
+        {
+          text: '<b>Start Transaction</b>',
+          type: 'button-positive',
+          onTap: function(e) {
+            alert("new Transaction");
+          }
+        }
+      ]
+    });
+    // $timeout(function() {
+    //    myPopup.close(); //close the popup after 3 seconds for some reason
+    // }, 3000);
+  }
+
+  $scope.error = function(m)
+  {
+    $scope.hide(); 
+    var alertPopup = $ionicPopup.alert({
+      title: 'Error',
+      template: '<pre><code>' + m + '</code></pre>'
+    });
+    alertPopup.then(function(res) {
+      console.log('Thank you for not eating my delicious ice cream cone');
+    });
+    $timeout(function() {
+       alertPopup.close(); //close the popup after 3 seconds for some reason
+    }, 3000);
+  }
+
+  $scope.create = function(m)
+  {
+    $scope.show();
+    $cordovaBarcodeScanner.scan().then(function(imageData) {
+      if (imageData.text.length > 0 && !imageData.cancelled)
+      {
+        m.qr = imageData;
+        m.qr.text = JSON.parse(m.qr.text.replace("id",'"id"').replace("deviceId",'"deviceId"'));
+        $cordovaPinDialog.prompt('Please enter a PIN (eg. 0000)').then(
+          function(result) {
+            if (result.buttonIndex == 1)
+            {
+              m.pin = result.input1;
+              if (m.pin.length >= 4)
+              {
+                $http.post(url + "/member/update/" + m.qr.text.id, m)
+                .then(function(response){
+                  // $scope.data = JSON.stringify(response.data,null,4);
+                  $scope.member = {};
+                  $scope.hide();  
+                  $scope.success();
+                },function(response){
+                  $scope.hide();
+                });
+              }
+              else
+              { 
+                $scope.error('Pin should at least be longer than 4 characters.');
+              }
+            }
+            else
+            { 
+              $scope.error('No pin code entered');
+            }
+          },
+          function (error) {
+            $scope.error(JSON.stringify(error,null,4));
+        });
+      }
+      else
+      {
+        $scope.error("QR code scanning cancelled");
+      }
+    }, function(error) {
+        $scope.error(JSON.stringify(error,null,4));
+    });
+  }
 })
 
 .controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
